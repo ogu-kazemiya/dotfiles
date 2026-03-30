c75c04f2b3694a6b1e7220ba842d98fd0ceb0c6a
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2026-03-31 00:11:42 +0900

    処理中メッセージを削除

--- setup.sh
+++ setup.sh
@@ -4,12 +4,6 @@ set -e
 
 # Homebrew
 if ! command -v brew &> /dev/null; then
-    echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    echo "Homebrew is already installed."
 fi
-
-echo "Installing packages from Brewfile..."
 brew bundle --file="$HOME/dotfiles/Brewfile"
-echo "Packages installed."
