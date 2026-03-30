b28c9fe8d2d349f1875025ee84e41ef127543821
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2026-03-31 01:45:35 +0900

    stowを使用可能に

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ if ! command -v brew &> /dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 brew bundle --file="$HOME/dotfiles/Brewfile"
+
+# stow
+stow --restow --dir="$HOME/dotfiles" --target="$HOME" git zsh
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -1,1 +1,5 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+if [ -f ~/.zprofile.local ]; then
+    source ~/.zprofile.local
+fi
